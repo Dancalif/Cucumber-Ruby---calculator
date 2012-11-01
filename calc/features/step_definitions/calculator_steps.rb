7f1cd63e8d1c2ab310d9ff9fa981c51574be7131
Dan <dancalif@gmail.com>
2012-11-01 00:14:43 -0700

    Second Commit

---
+++ calc/features/step_definitions/calculator_steps.rb
@@ -0,0 +1,15 @@
+
+Given /^the input "([^"]*)"$/ do |input|
+  @input = input
+end
+
+When /^the calculator is run$/ do
+  @output = `ruby calc.rb #{@input}`.to_f.round(3).to_s.chop.to_f
+  raise('Command failed!') unless $?.success?
+end
+
+Then /^the output should be "([^"]*)"$/ do |expected_output|
+#print output
+  @output.should == expected_output.to_f.round(2)
+
+end
